c2cd7c8696f7905f09b6667285646aa08e0d9756
Allan Ferreira <al.portoferreira@gmail.com>
2025-05-13 17:33:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// Dark / Light Mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+    setDarkMode();
+}
+
+function setDarkMode() {
+    // Some Logic
+}
+function setLightDarkMode() {
+    // Some Logic
+}
