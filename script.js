3a2ca0372818db04323c5ed3106400a959cee03e
Allan Ferreira <al.portoferreira@gmail.com>
2025-05-20 12:11:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ function toggleMenu() {
 const btn = document.getElementById("modeToggle");
 const btn2 = document.getElementById("modeToggle2");
 const themeIcons = document.querySelectorAll(".icon");
-const currentTheme = localStorage.getItem("theme");
+const currentTheme = localStorage.getItem("t    heme");
 
 if (currentTheme === "dark") {
     setDarkMode();
 }
 
-btn.addEventListener("click", fuction() {
+btn.addEventListener("click", function() {
     setTheme();
 });
-btn2.addEventListener("click", fuction() {
+btn2.addEventListener("click", function() {
     setTheme();
 });
 
